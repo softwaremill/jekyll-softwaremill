89a5f91931d2f98eb9502324e7e48bbbf0a85054
Nowaker <damian.nowak@geozone.pl>
2013-09-27 18:55:34 +0200

    Twitter feeds on home and blog - extracted Twitter-ralated code from templates to standalone tools

---
+++ __generators/config.php
@@ -0,0 +1,88 @@
+<?php
+return array(
+  'Adam Warski' => array(
+    'twitter' => 'adamwarski',
+    'blog' => 'http://www.warski.org/blog/',
+  ),
+  'Aleksandra Puchta' => array(
+    'twitter' => 'PuchtaOla',
+    'blog' => NULL,
+  ),
+  'Damian Nowak' => array(
+    'twitter' => 'nwkr',
+    'blog' => 'http://www.nowaker.net/',
+  ),
+  'Jacek Kunicki' => array(
+    'twitter' => 'rucek',
+    'blog' => 'http://blog.kunicki.org',
+  ),
+  'Jan Zborowski' => array(
+    'twitter' => NULL,
+    'blog' => NULL,
+  ),
+  'Jarosław Kijanowski' => array(
+    'twitter' => 'jkijanowski',
+    'blog' => 'http://kijanowski.blogspot.com/',
+  ),
+  'Kinga Pukianiec' => array(
+    'twitter' => NULL,
+    'blog' => NULL,
+  ),
+  'Krzysztof Ciesielski' => array(
+    'twitter' => 'kpciesielski',
+    'blog' => 'http://abstractionextraction.wordpress.com/',
+  ),
+  'Krzysztof Grajek' => array(
+    'twitter' => 'grajo',
+    'blog' => 'http://www.softwarepassion.com/',
+  ),
+  'Łukasz Lenart' => array(
+    'twitter' => 'lukaszlenart',
+    'blog' => 'http://dailylog.lenart.org.pl/',
+  ),
+  'Łukasz Żuchowski' => array(
+    'twitter' => 'zuchos',
+    'blog' => 'http://blog.zuchos.com/',
+  ),
+  'Maciej Biłas' => array(
+    'twitter' => 'maciejb',
+    'blog' => 'http://maciej.inszy.org/',
+  ),
+  'Maciej Hnydka' => array(
+    'twitter' => NULL,
+    'blog' => NULL,
+  ),
+  'Małgorzata Wojciechowska' => array(
+    'twitter' => NULL,
+    'blog' => NULL,
+  ),
+  'Michał Matłoka' => array(
+    'twitter' => 'mmatloka',
+    'blog' => NULL,
+  ),
+  'Michał Ostruszka' => array(
+    'twitter' => 'mostruszka',
+    'blog' => 'http://michalostruszka.pl',
+  ),
+  'Paweł Stawicki' => array(
+    'twitter' => 'pawelstawicki',
+    'blog' => 'http://pawelstawicki.blogspot.com/',
+  ),
+  'Paweł Wrzeszcz' => array(
+    'twitter' => 'pawelwrzeszcz',
+    'blog' => 'http://pawel.wrzesz.cz/',
+  ),
+  'Piotr Buda' => array(
+    'twitter' => 'piotrbuda',
+    'blog' => 'http://piotrbuda.eu/',
+  ),
+  'Tomasz Dziurko' => array(
+    'twitter' => 'TomaszDziurko',
+    'blog' => 'http://tomaszdziurko.pl/',
+  ),
+  'Tomasz Szymański' => array(
+    'twitter' => 'szimano',
+    'blog' => 'http://szimano.org/',
+  ),
+);
+?>
