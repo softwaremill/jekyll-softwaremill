912d718a8dda0921ab492e157d1b04cb9886123b
Nowaker <damian.nowak@geozone.pl>
2013-10-03 13:04:18 +0200

    simple url rewrite in python server

--- __proxy/server.py
+++ __proxy/server.py
@@ -3,7 +3,20 @@
 import BaseHTTPServer
 import SimpleHTTPServer
 
+
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     error_message_format = open("404.html", "r").read()
+
+    routes = {
+        "/wp-content/uploads/": "/img/uploads/"
+    }
+
+    def do_GET(self):
+        for before, after in self.routes.items():
+            if self.path.startswith(before):
+                self.path = after + self.path[before.__len__():]
+
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
 
 BaseHTTPServer.test(MyHandler, BaseHTTPServer.HTTPServer)
