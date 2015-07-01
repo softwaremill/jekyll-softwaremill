8e23075b5f2acbad6ef11e6099482202ba7eb1b0
Michal Matloka <michal.matloka@softwaremill.com>
2015-07-01 12:29:46 +0200

    Vagrant - fix for rvm gpg retrieval

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 sudo apt-get -y install php5-cli imagemagick libmagickwand-dev libcurl3 libcurl3-gnutls libcurl4-openssl-dev
 
 # GPG key for RVM installation
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
 echo "Getting rvm"
 curl -sSL https://get.rvm.io | bash -s stable
